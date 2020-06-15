64ac9da4b6f3abf02cd00bc71d42dc196f8c65bc
ssssota <tommysouta@gmail.com>
2020-06-15 09:19:10 +0900

    fix fonts test

--- test/test.js
+++ test/test.js
@@ -2,14 +2,19 @@
 eslint-disable 
   @typescript-eslint/no-var-requires,
   @typescript-eslint/no-unsafe-member-access,
-  @typescript-eslint/no-unsafe-call
+  @typescript-eslint/no-unsafe-call,
+  @typescript-eslint/no-unsafe-assignment,
+  @typescript-eslint/restrict-template-expressions
 */
 const assert = require('assert');
+const fs = require('fs');
 const path = require('path');
 const fontscan = require('../dist/index');
 const { FontDescriptor } = require('../dist/fontDescriptor');
+const expected = require('./expected.json');
 
 const isFontDescriptor = (fd) => {
+  assert.ok(fs.existsSync(fd.path));
   assert.equal(typeof fd.path, 'string');
   assert.equal(typeof fd.postscriptName, 'string');
   assert.equal(typeof fd.family, 'string');
@@ -20,9 +25,6 @@ const isFontDescriptor = (fd) => {
   assert.equal(typeof fd.monospace, 'boolean');
 };
 
-// todo: テストデータの正解の値をまとめる
-// todo: ディレクトリは__dirnameから生成する
-// todo: テストデータはisFontDescriptorとまとめた正解値をdeepEqualする
 // todo: 各関数の型エラーチェックをする
 
 /*
@@ -39,85 +41,15 @@ describe('FontDescriptor', function () {
   });
 
   describe('#createFromPath', function () {
-    it('should return FontDescriptor', () => {
-      const fd = FontDescriptor.createFromPath(
-        './test/data/mplus-2m-regular.ttf'
-      );
-      isFontDescriptor(fd);
-    });
-
-    it('should return correct status(M+ 2m Regular)', () => {
-      const fd = FontDescriptor.createFromPath(
-        './test/data/mplus-2m-regular.ttf'
-      );
-      assert.ok(
-        fd.path.replace(/\\/g, '/').endsWith('/test/data/mplus-2m-regular.ttf')
-      );
-      assert.equal(fd.family, 'M+ 2m');
-      assert.equal(fd.postscriptName, 'mplus-2m-regular');
-      assert.equal(fd.style, 'Regular');
-      assert.equal(fd.weight, 400);
-      assert.equal(fd.width, 5);
-      assert.equal(fd.italic, false);
-      assert.equal(fd.monospace, false);
-    });
-    it('should return correct status(M+ 1c Light)', () => {
-      const fd = FontDescriptor.createFromPath(
-        './test/data/mplus-1c-light.ttf'
-      );
-      assert.ok(
-        fd.path.replace(/\\/g, '/').endsWith('/test/data/mplus-1c-light.ttf')
-      );
-      assert.equal(fd.family, 'M+ 1c light');
-      assert.equal(fd.postscriptName, 'mplus-1c-light');
-      assert.equal(fd.style, 'Regular');
-      assert.equal(fd.weight, 300);
-      assert.equal(fd.width, 5);
-      assert.equal(fd.italic, false);
-      assert.equal(fd.monospace, false);
-    });
-    it('should return correct status(M+ 2p Black)', () => {
-      const fd = FontDescriptor.createFromPath(
-        './test/data/mplus-2p-black.ttf'
-      );
-      assert.ok(
-        fd.path.replace(/\\/g, '/').endsWith('/test/data/mplus-2p-black.ttf')
-      );
-      assert.equal(fd.family, 'M+ 2p black');
-      assert.equal(fd.postscriptName, 'mplus-2p-black');
-      assert.equal(fd.style, 'Regular');
-      assert.equal(fd.weight, 900);
-      assert.equal(fd.width, 5);
-      assert.equal(fd.italic, false);
-      assert.equal(fd.monospace, false);
-    });
-    it('should return correct status(Roboto)', () => {
-      const fd = FontDescriptor.createFromPath(
-        './test/data/Roboto-Regular.ttf'
-      );
-      assert.ok(
-        fd.path.replace(/\\/g, '/').endsWith('/test/data/Roboto-Regular.ttf')
-      );
-      assert.equal(fd.family, 'Roboto');
-      assert.equal(fd.postscriptName, 'Roboto-Regular');
-      assert.equal(fd.style, 'Regular');
-      assert.equal(fd.weight, 400);
-      assert.equal(fd.width, 5);
-      assert.equal(fd.italic, false);
-      assert.equal(fd.monospace, false);
-    });
-    it('should return correct status(Roboto Italic)', () => {
-      const fd = FontDescriptor.createFromPath('./test/data/Roboto-Italic.ttf');
-      assert.ok(
-        fd.path.replace(/\\/g, '/').endsWith('/test/data/Roboto-Italic.ttf')
-      );
-      assert.equal(fd.family, 'Roboto');
-      assert.equal(fd.postscriptName, 'Roboto-Italic');
-      assert.equal(fd.style, 'Italic');
-      assert.equal(fd.weight, 400);
-      assert.equal(fd.width, 5);
-      assert.equal(fd.italic, true);
-      assert.equal(fd.monospace, false);
+    expected.fonts.map((fontdata) => {
+      it(`should return correct status ${fontdata.name}`, () => {
+        const fd = FontDescriptor.createFromPath(
+          path.join('test', fontdata.path)
+        );
+        isFontDescriptor(fd);
+        fontdata.path = path.join(__dirname, fontdata.path);
+        assert.deepEqual(fd, fontdata);
+      });
     });
   });
 });
